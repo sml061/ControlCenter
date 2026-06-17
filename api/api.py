a5aed025cf8b46befacf1125b0e2e6fce52deeb4
Samuel Bonfim <samuelbonfimaraujo9210@gmail.com>
2026-06-17 14:27:00 -0300

    Encrypt function. Add function fixed. Password and login add.

--- api/api.py
+++ api/api.py
@@ -1,5 +1,6 @@
 import json
 import sqlite3
+import bcrypt
 from datetime import datetime, date
 
 class Aluno():
@@ -18,24 +19,51 @@ class Aluno():
   cursor.execute("""
 CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL UNIQUE,
+    login TEXT NOT NULL UNIQUE,
+    name TEXT NOT NULL,
     email TEXT NOT NULL UNIQUE,
-    phone TEXT NOT NULL UNIQUE,
+    phone TEXT NOT NULL,
     birthday TEXT NOT NULL,
-    is_student INTEGER NOT NULL
+    is_student INTEGER NOT NULL,
+    password TEXT NOT NULL
 );
 """)
   
   @staticmethod
-  def add(name, email, phone, birthday, is_student):  # ---> Make a function for add a new Person
-    try:
+  def encrypt(senha):
+    hash_senha = bcrypt.hashpw(senha.encode(), bcrypt.gensalt()).decode()
+    return hash_senha
 
+  @staticmethod
+  def add(login, name, email, phone, birthday, is_student, passwd):  # ---> Make a function for add a new Person
+    nascimento = datetime.strptime(birthday, "%d/%m/%Y").date()
+    if (nascimento.year > date.today().year) or (nascimento.year < (date.today().year - 130)):
+      print("Data de aniversario invalida")
+    elif (is_student != "1") and (is_student != "0"):
+      print("Valor invalido do campo: is_student")
+    else:
+      try:
+        user_data = (login, name, email, phone, birthday, is_student, Aluno.encrypt(passwd))
+        Aluno.cursor.execute("INSERT INTO users (login, name, email, phone, birthday, is_student, password) VALUES (?, ?, ?, ?, ?, ?, ?)", user_data)
+        Aluno.conn.commit()
+      except Exception as e:
+        if str(e) == "UNIQUE constraint failed: users.email":
+          print("Email existente")
+        elif str(e) == "UNIQUE constraint failed: users.login":
+          print("Login existente")
+        else:
+          print(f"ERRO: {e}")
 
-      user_data = (name, email, phone, birthday, is_student)
-      Aluno.cursor.execute("INSERT INTO users (name, email, phone, birthday, is_student) VALUES (?, ?, ?, ?, ?)", user_data)
-      Aluno.conn.commit()
-    except Exception as e:
-      print(f"Erro ao criar novo usuario {e}")
+  @staticmethod
+  def login(login, passwd):
+    Aluno.cursor.execute("SELECT * FROM users WHERE login = ?", (login,))
+    row = Aluno.cursor.fetchone()
+    if row:
+      hash_salvo = row[7]
+      if bcrypt.checkpw(passwd.encode(), hash_salvo.encode()):
+        print("OK")
+      else:
+        print("Senha ou login incorreto")
   
   @staticmethod
   def calcular_idade(data_str):
@@ -55,5 +83,21 @@ CREATE TABLE IF NOT EXISTS users (
     rows = Aluno.cursor.fetchall()
 
     for row in rows:
-      idade = Aluno.calcular_idade(row[4])
-      print(f"ID: {row[0]}, Name: {row[1]}, Email: {row[2]}, Phone: {row[3]}, BirthDay: {row[4]}, Age: {idade}")
+      idade = Aluno.calcular_idade(row[5])
+      print(f"ID: {row[0]}, Login: {row[1]}, Name: {row[2]}, Email: {row[3]}, Phone: {row[4]}, BirthDay: {row[5]}, Age: {idade}")
+  
+  @staticmethod
+  def delete(id):
+    try:
+      Aluno.cursor.execute("DELETE FROM users WHERE id = ?", (id,))
+      Aluno.conn.commit()
+    except Exception as e:
+      print(f"Erro: {e}")
+  
+  @staticmethod
+  def recuperarSenha(login, passwd):
+    try:
+      Aluno.cursor.execute("UPDATE users SET password = ? WHERE login = ?", (Aluno.encrypt(passwd), login,))
+      Aluno.conn.commit()
+    except Exception as e:
+      print(f"ERRO: {e}")
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from api.api import Aluno
 
 def Main():
-  Aluno.add("Samuel Bonfim Araujo", "samuelbonfimaraujo@gmail.com", "61996445622", "09/02/2010", "1")
-  Aluno.show()
+  # Aluno.add("SamuelDF", "Samuel Bonfim Araujo", "samuelbonfimaraujo@gmail.com", "61984132047", "09/02/1982", "1", "1234")
+  # Aluno.login("SamuelDF", "123456")
+  # Aluno.recuperarSenha("SamuelDF", "123456")
+  # Aluno.delete(1)
+  # Aluno.show()
 
 if __name__ == "__main__":
   Main()
